5982cf9be3e59fe0b25511cc9f83f1f3fef9b5d5
lucasmaciel404 <maciellucas487@gmail.com>
2025-05-30 11:06:39 -0300

    feat: liberando cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // configuração do validators presentes nos DTO's
-    app.useGlobalPipes(
+  app.enableCors();
+
+  app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
-  
+
   const config = new DocumentBuilder()
     .setTitle('Avantsoft API')
     .setDescription('API para gerenciamento de produtos')
@@ -22,7 +23,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('v1/doc', app, document); 
+  SwaggerModule.setup('v1/doc', app, document);
 
   await app.listen(3000);
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -43,7 +43,6 @@ export class ProductsService {
   }
 
   async update(id: number, data: UpdateProductDto): Promise<Product> {
-    await this.validateSku(data.sku);
 
     const product = await this.findOne(id);
 
